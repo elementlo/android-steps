e531d08142676590c8f7d8c606ca2a82b5cde2d6
Elemen <lawliet.zhan@outlook.com>
2025-04-02 23:07:45 +0800

    feat: add more features of ConstraintLayout.

--- app/src/main/java/com/elementlo/android_steps/MainActivity.kt
+++ app/src/main/java/com/elementlo/android_steps/MainActivity.kt
@@ -3,14 +3,10 @@ package com.elementlo.android_steps
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-/**
- * @author: dynamsoft
- * Time: 2025/4/2
- * Description:
- */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
