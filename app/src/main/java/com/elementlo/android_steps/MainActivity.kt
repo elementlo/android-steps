cc998c379d052dc4f3942139bbed3d78eff09147
Elemen <lawliet.zhan@outlook.com>
2025-04-03 13:43:11 +0800

    feat: add more features.

--- app/src/main/java/com/elementlo/android_steps/MainActivity.kt
+++ app/src/main/java/com/elementlo/android_steps/MainActivity.kt
@@ -1,12 +1,36 @@
 package com.elementlo.android_steps
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.constraintlayout.widget.ConstraintSet
+import androidx.constraintlayout.widget.Placeholder
+import androidx.transition.TransitionManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+    }
+
+    fun onClick(view: View) {
+        findViewById<Placeholder>(R.id.placeholder).setContentId(view.id)
+    }
+
+    fun onConstraintSetClick(view: View) {
+        val constraintLayout = view as ConstraintLayout
+
+        val constraintSet = ConstraintSet().apply {
+            //hard checking the view id matching.
+            isForceId = false
+            clone(this@MainActivity,
+                R.layout.layout_set_to
+            )
+        }
+        //Add an animation to the layout.
+        TransitionManager.beginDelayedTransition(constraintLayout)
+        constraintSet.applyTo(constraintLayout)
     }
 }
